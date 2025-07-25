95ea06f78a6cdf93f4edf27c0bb617841a57183a
lucacodorean <codoreanluca@gmail.com>
2025-07-26 00:07:37 +0300

    feat: Proper parameter type for key-based get methods with different key types.

--- src/Utils/ParameterConverter.php
+++ src/Utils/ParameterConverter.php
@@ -4,5 +4,22 @@ namespace Src\Utils;
 
 class ParameterConverter
 {
+    public static function setProperIdentifier(array $firstLine, string $value): string|int {
+        $properIdentifier = $value;
+        $header = HeaderWorker::computeHeader($firstLine);
 
+        if($header == [] && !is_numeric($value)) {
+            echo "Can't operate with a column name. The data table has no header.";
+            exit;
+        }
+
+        else if(is_numeric($value) && $header != [])  {
+            $keys = array_keys($header);
+            if(isset($keys[$value])) {
+                $properIdentifier =  $keys[$value];
+            }
+        }
+
+        return is_numeric($properIdentifier) ? (int) $properIdentifier : $properIdentifier;
+    }
 }
