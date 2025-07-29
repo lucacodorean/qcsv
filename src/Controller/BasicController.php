ae0f08bd370085e16a31c7563e77ecbfcc00e9a3
lucacodorean <codoreanluca@gmail.com>
2025-07-30 01:00:39 +0300

    feat: Added a route that displays request data using a JSON.

--- src/Controller/BasicController.php
+++ src/Controller/BasicController.php
@@ -5,6 +5,7 @@ namespace Src\Controller;
 use Random\RandomException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -33,4 +34,20 @@ class BasicController extends AbstractController {
             $this->homePath,
         );
     }
+
+    #[Route("/print-request", name: 'print-request')]
+    public function printRequest(Request $request): Response {
+        return $this->json(
+            data: [
+                "host" => $request->getHost(),
+                "port" => $request->getPort(),
+                "method" => $request->getMethod(),
+                "path" => $request->getPathInfo(),
+                "headers" => $request->headers->all(),
+            ],
+            context: [
+            'json_encode_options' => JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        ]);
+    }
+
 }
