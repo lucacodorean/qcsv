09f4e4261f3565ed88d5ac4eac7be430102d3c7e
lucacodorean <codoreanluca@gmail.com>
2025-07-30 00:48:26 +0300

    feat: Added a route that redirects to an external page.

--- src/Controller/BasicController.php
+++ src/Controller/BasicController.php
@@ -2,13 +2,34 @@
 
 namespace Src\Controller;
 
+use Random\RandomException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class BasicController
-{
+class BasicController extends AbstractController {
+
+    public function __construct(
+        #[Autowire('%env(HOME_PATH)%')] private readonly string $homePath
+    ) {
+        ///
+    }
+
     #[Route('/', name: 'homepage')]
     public function index(): Response {
         return new Response('It works!');
+    }
+
+    /**
+     * @throws RandomException
+     */
+    #[Route('/go', name: 'redirect')]
+    public function go(): Response {
+        sleep(random_int(1, 3));
+
+        return $this->redirect(
+            $this->homePath,
+        );
     }
 }
